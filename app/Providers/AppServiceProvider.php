24c0fd7c14aeb7df390b9c58eba703e5ef9127e4
Ramil Zaitov <ramoff9@gmail.com>
2024-12-09 19:33:56 +0400

    Add authentication bootstrap ui

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,15 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Models\Task' => 'App\Policies\TaskPolicy',
+    ];
+
+    /**
      * Register any application services.
      */
     public function register(): void
